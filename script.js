292f2b2f7d4f418b2b7668ebb58b69ab01e37be3
Liam Howatt <liamhowatt34@gmail.com>
2024-01-03 13:37:44 -0500

    Added list smooth animation

--- script.js
+++ script.js
@@ -2,28 +2,15 @@ const expList = document.getElementById('exp-list');
 const incList = document.getElementById('inc-list');
 const expHideShowButton = document.getElementById('exp-hide-show');
 const incHideShowButton = document.getElementById('inc-hide-show');
-let expDisplay = 0;
-let incDisplay = 0;
 
 function toggleExpList() {
-    if (expDisplay == 1) {
-        expList.style.display = 'block';
-        expDisplay = 0;
-    } else {
-        expList.style.display = 'none';
-        expDisplay = 1;
-    }
+    expList.classList.toggle('expanded');
 }
 
 function toggleIncList() {
-    if (incDisplay == 1) {
-        incList.style.display = 'block';
-        incDisplay = 0;
-    } else {
-        incList.style.display = 'none';
-        incDisplay = 1;
-    }
+    incList.classList.toggle('expanded');
 }
 
 expHideShowButton.addEventListener('click', toggleExpList);
 incHideShowButton.addEventListener('click', toggleIncList);
+
