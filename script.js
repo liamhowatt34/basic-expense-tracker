91f4ff7c14ab214de46d33bb6eb71981d8660a58
Liam Howatt <liamhowatt34@gmail.com>
2024-01-03 17:19:06 -0500

    Added input field functionality with the add and remove buttons

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const expList = document.getElementById('exp-list');
 const incList = document.getElementById('inc-list');
 const expHideShowButton = document.getElementById('exp-hide-show');
 const incHideShowButton = document.getElementById('inc-hide-show');
+const expInputDesc = document.getElementById('exp-desc');
+const expInputAmt = document.getElementById('exp-amt');
+const expAddBtn = document.getElementById('exp-add');
+const expRemoveBtn = document.getElementById('exp-rmv');
 
+
+// expanding list
 function toggleExpList() {
     expList.classList.toggle('expanded');
 }
@@ -14,3 +20,28 @@ function toggleIncList() {
 expHideShowButton.addEventListener('click', toggleExpList);
 incHideShowButton.addEventListener('click', toggleIncList);
 
+
+// input info
+function appendExpList() {
+    let liElement = document.createElement("li");
+    liElement.textContent = `- ${expInputDesc.value}, $${expInputAmt.value}`;
+    expList.appendChild(liElement);
+
+    liElement.addEventListener('click', function () {
+        liElement.classList.toggle('selected');
+    });
+
+    expInputDesc.value = '';
+    expInputAmt.value = '';
+}
+
+function removeExpList() {
+    let selectedItems = expList.querySelectorAll('.selected');
+
+    selectedItems.forEach(function (item) {
+        expList.removeChild(item);
+    });
+}
+
+expRemoveBtn.addEventListener('click', removeExpList);
+expAddBtn.addEventListener('click', appendExpList);
